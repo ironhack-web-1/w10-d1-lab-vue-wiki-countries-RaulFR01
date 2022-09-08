c6f7643c0edc3f769377ab2cdbeb10ed71be5276
RaulFR01 <rferrazrobles@hotmail.com>
2022-09-08 21:05:17 +0200

    iteration 1.1 done

---
+++ src/components/NavBar.vue
@@ -0,0 +1,11 @@
+<template>
+  <nav class="navbar navbar-dark bg-primary mb-3">
+    <div class="container">
+      <a class="navbar-brand" href="/">LAB | Vue.js WikiCountries</a>
+    </div>
+  </nav>
+</template>
+
+<script>
+export default {};
+</script>
